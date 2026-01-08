6f64c7de8a4cea3535af9776b4184602f3ff3bf9
Albertico <chopytrollface@gmail.com>
2026-01-08 17:44:22 -0600

    Commit 01/08/26 #1
    1. Nuevo campo para poner el nombre 2. Metodo para cargar los datos

--- src/main/java/com/albalatro/controller/SalarioController.java
+++ src/main/java/com/albalatro/controller/SalarioController.java
@@ -2,6 +2,7 @@ package com.albalatro.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 import com.albalatro.model.Pago;
 import com.albalatro.model.Salario;
@@ -15,11 +16,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
 
 public class SalarioController implements Initializable{
     @FXML private Label labelNormal, labelDomingo;
-    @FXML private CheckBox choiceTodos;
+    @FXML private TextField fieldNombre;
     @FXML private RadioButton choiceHora, choiceFijo;
+    @FXML private CheckBox choiceTodos;
     @FXML private Spinner<Double> spinnerNormal, spinnerDomingo;
     @FXML private Button btnGuardar, btnCancelar;
     private Salario salario;
@@ -31,9 +34,48 @@ public class SalarioController implements Initializable{
         new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 999.99);
         
         valueFactory.setValue(40.0);
-        
         spinnerNormal.setValueFactory(valueFactory);
+
+        valueFactory.setValue(50.0);
         spinnerDomingo.setValueFactory(valueFactory);
+        
+    }
+    
+    public void cargarDatos(Salario salario) {
+        this.salario = salario;
+        
+        if (salario != null) {
+            switch (salario.getPago()) {
+                case Pago.HORA -> choiceHora.setSelected(true);
+                case Pago.FIJO -> choiceFijo.setSelected(true);
+            }
+            fieldNombre.setText(salario.getNombre());
+            
+            if(salario.getNormal().equals(salario.getDomingo())) {
+                choiceTodos.setSelected(true);
+            }
+            
+            SpinnerValueFactory<Double> valueFactory = 
+            new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 999.99);
+            valueFactory.setValue(salario.getNormal());
+            spinnerNormal.setValueFactory(valueFactory);
+            
+            valueFactory.setValue(salario.getDomingo());
+            spinnerDomingo.setValueFactory(valueFactory);
+        } else {
+            salario = new Salario();
+            salario.setId(UUID.randomUUID().toString());
+            
+            salario.setPago(Pago.HORA);
+            choiceHora.setSelected(true);
+
+            salario.setNombre("Nuevo salario");
+            fieldNombre.setText(salario.getNombre());
+
+            salario.setNormal(spinnerNormal.getValue());
+            salario.setDomingo(spinnerDomingo.getValue());
+        }
+        spinnerShow();
         
     }
     
@@ -49,6 +91,10 @@ public class SalarioController implements Initializable{
     
     @FXML
     public void setDias(ActionEvent event) {
+        spinnerShow();
+    }
+    
+    private void spinnerShow(){
         if(choiceTodos.isSelected()) {
             labelNormal.setText("Todos los días");
             labelDomingo.setVisible(false);
@@ -60,11 +106,19 @@ public class SalarioController implements Initializable{
             
         }
     }
-
+    
     public void guardar() {
+        salario.setNombre(fieldNombre.getText());
 
+        salario.setNormal(spinnerNormal.getValue());
+        
+        if(choiceTodos.isSelected()) 
+            salario.setDomingo(spinnerNormal.getValue());
+        else
+            salario.setDomingo(spinnerDomingo.getValue()); 
+        
     }
-
+    
     public void cancelar() {
         
     }
